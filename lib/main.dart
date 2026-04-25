87a269a49617e469d6ce8c0c38ae9794f3872f44
Useless8000B <adrielluiz73@gmail.com>
2026-04-24 22:10:27 -0300

    feat: Initialize Firebase and implement user authentication flow in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:threads_app/colors.dart';
+import 'package:threads_app/firebase_options.dart';
+import 'package:threads_app/screens/login_screen.dart';
 import 'package:threads_app/screens/shell_screen.dart';
+import 'package:threads_app/services/auth_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Threads());
 }
 
@@ -15,26 +21,47 @@ class Threads extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Threads',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.white, brightness: Brightness.dark),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.white,
+          brightness: Brightness.dark,
+        ),
 
         appBarTheme: AppBarThemeData(
           backgroundColor: AppColors.black,
-          titleTextStyle: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 24
-          )
+          titleTextStyle: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
         ),
         navigationBarTheme: NavigationBarThemeData(
           backgroundColor: AppColors.black,
           labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
           iconTheme: WidgetStateProperty.fromMap({
-            WidgetState.selected: const IconThemeData(size: 28, color: Colors.white),
-            WidgetState.any: const IconThemeData(size: 28, color: AppColors.grey),
+            WidgetState.selected: const IconThemeData(
+              size: 28,
+              color: Colors.white,
+            ),
+            WidgetState.any: const IconThemeData(
+              size: 28,
+              color: AppColors.grey,
+            ),
           }),
         ),
-        scaffoldBackgroundColor: AppColors.black
+        scaffoldBackgroundColor: AppColors.black,
       ),
-      home: const ShellScreen(),
+      home: StreamBuilder(
+        stream: AuthService().user,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const ShellScreen();
+          }
+
+          return const LoginScreen();
+        },
+      ),
     );
   }
 }
