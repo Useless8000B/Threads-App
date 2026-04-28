81c7680446d53d5c1af45f184f46a2a8a939bf69
Useless8000B <adrielluiz73@gmail.com>
2026-04-28 15:25:24 -0300

    Remove unused test

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:threads_app/main.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const Threads());
-  });
-}
