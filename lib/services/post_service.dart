5512deb3acc08d5406cc1d7c8b002f3dd2477fe5
Useless8000B <adrielluiz73@gmail.com>
2026-04-27 21:06:54 -0300

    feat: enhance PostService to include user authentication token for post creation and retrieval

--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -1,33 +1,50 @@
 import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:threads_app/models/post_model.dart';
+import 'package:threads_app/services/auth_service.dart';
 
 class PostService {
   static const String _baseUrl = String.fromEnvironment("API_URL");
+  static final AuthService _authService = AuthService();
 
   Future<bool> createPost(PostModel post) async {
     try {
+      String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
+
       final response = await http.post(
-        Uri.parse(_baseUrl),
+        Uri.parse("$_baseUrl/posts"),
         headers: {
           "Content-Type": "application/json",
+          "Authorization": "Bearer $token",
         },
         body: jsonEncode(post.toJson()),
       );
 
-      if (response.statusCode == 201) {
+      if (response.statusCode == 201 || response.statusCode == 200) {
         return true;
       } else {
-        throw Exception("Backend error (${response.statusCode}): ${response.body}");
+        print("Erro ao criar: ${response.statusCode} - ${response.body}");
+        return false;
       }
     } catch (e) {
-      throw Exception("Connection failed: $e");
+      return false;
     }
   }
 
   Future<List<PostModel>> fetchPosts() async {
     try {
-      final response = await http.get(Uri.parse("$_baseUrl/posts"));
+      String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
+
+      final headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      };
+
+      final response = await http.get(
+        Uri.parse("$_baseUrl/posts"),
+        headers: headers,
+      );
 
       if (response.statusCode == 200) {
         List<dynamic> body = jsonDecode(response.body);
@@ -37,6 +54,31 @@ class PostService {
       }
     } catch (e) {
       throw Exception("Error retrieving posts");
+    }
+  }
+
+  Future<List<PostModel>> getPostsByUser() async {
+    try {
+      final token = await _authService.token;
+
+      if (token == null) {
+        throw Exception("User not authenticated");
+      }
+
+      final response = await http.get(
+        Uri.parse("$_baseUrl/posts/me"),
+        headers: {"Authorization": "Bearer $token"},
+      );
+
+      if (response.statusCode == 200) {
+        final String decodedBody = utf8.decode(response.bodyBytes);
+        List<dynamic> body = jsonDecode(decodedBody);
+        return body.map((item) => PostModel.fromJson(item)).toList();
+      }
+
+      return [];
+    } catch (e) {
+      return [];
     }
   }
 }
