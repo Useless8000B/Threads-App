1da3876f0b515b5584326095bdedab8313a683c9
Useless8000B <adrielluiz73@gmail.com>
2026-05-01 19:35:09 -0300

    Add new color

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,4 +5,6 @@ class AppColors {
   static const Color black = Color(0xFF000000);
   static const Color grey = Color(0xFF757575);
   static const Color grey_1 = Color(0xFF666666);
+  static const Color grey_2 = Color(0xFF1F1F1F);
+  static const Color grey_3 = Colors.grey;
 }
